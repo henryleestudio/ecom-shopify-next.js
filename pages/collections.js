52d9fd0dfd4d6914d7caf1c894624346ddf4dfc4
Henry Lee Studio <user@Henry-Lee-i9-16-MacBook-Pro.local>
2023-12-20 10:48:36 -0500

    shopify template

--- pages/collections.js
+++ pages/collections.js
@@ -8,7 +8,7 @@ export default function Collections() {
   return (
     <MainLayout>
     <Head>
-      <title>Create Next App</title>
+      <title>The Co – Creative Collections</title>
         <meta name="description" content="By cr>eate Next App" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
